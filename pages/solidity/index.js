09a786900fbc7982a038f0324199d6cf09590178
Moto Sakanosita <moto@sakanosita.org>
2022-01-10 00:24:14 +0900

    chore

--- pages/solidity/index.js
+++ pages/solidity/index.js
@@ -1,8 +1,81 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Layout from '../../components/layout'
+import { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { ethers } from 'ethers'
 
-function Solidity() {
+let	provider;
+let	signer;
+
+export default function Home() {
+
+	const [state, setState] = useState({
+		account: null,
+		ethBalance: '-',
+		connectButton: false,
+		hasMetaMask: true,
+	});
+
+	const router = useRouter();
+
+	useEffect(() => {
+		
+    provider = new ethers.providers.Web3Provider(window.ethereum, "any");
+
+		if (sessionStorage["SandboxMetaAlreadyConnected"])
+			connectMeta();
+		else
+			setState({...state, connectButton: true});
+
+		window.ethereum.on('accountsChanged', () => {
+			connectMeta();
+		});
+
+		window.ethereum.on('chainChanged', () => {
+			document.location.reload();
+		});
+
+	}, []);
+
+	async function connectMeta() {
+		if (!window.ethereum) {
+			window.alert('You must install MetaMask to use this website');
+			return;
+		}
+
+		let network = await provider.getNetwork();
+
+		if (network.chainId == 1) {
+			router.push('/');
+			return;
+		} else if (network.chainId  != 4) {
+			window.alert('This website only supports Mainnet or Rinkeby testnet');
+			return;
+		}
+		
+		// Prompt user for account connections
+		await provider.send("eth_requestAccounts", []);
+		signer = provider.getSigner();
+		sessionStorage["SandboxMetaAlreadyConnected"] = true;
+
+		const [signerAddress] = await Promise.all([
+													signer.getAddress(),
+													]);
+		
+		const [signerBalance] = await Promise.all([
+													getEthBalance(signerAddress, provider), 
+													]);
+
+		setState({
+			...state,
+			account: signerAddress,
+			ethBalance: signerBalance,
+			connectButton: false,
+		});
+		return;
+	}
+
   return (
     <Layout>
       <Head>
@@ -19,7 +92,10 @@ function Solidity() {
             <h2 class="card-title">First Application
             </h2> 
             <div class="justify-end card-actions">
-              <button class="btn btn-secondary">Go</button>
+              <button class="btn btn-secondary">inc</button>
+            </div>
+            <div class="justify-end card-actions">
+              <button class="btn btn-secondary">dec</button>
             </div>
           </div>
         </div>
@@ -27,5 +103,3 @@ function Solidity() {
     </Layout>
   )
 }
-  
-export default Solidity
